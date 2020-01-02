3f8a29ebf89398567cadd49f5b4d7444b36e166d
Roberts Sukonovs <roberts.sukonovs@gmail.com>
2020-01-02 15:40:03 +0200

    Add getters and setters.

--- address.php
+++ address.php
@@ -1,13 +1,165 @@
 <?php
 
-class Address {
+class Address
+{
+    /**
+     * @var string
+     */
+    private $houseNumber = '';
 
-	private $houseNumber = '';
-	private $street = '';
-	protected $city = '';
-	protected $county = '';
-	public $postcode = '';
-	public $country = '';
-	private $contacts = [];
+    /**
+     * @var string
+     */
+    private $street = '';
 
+    /**
+     * @var string
+     */
+    protected $city = '';
+
+    /**
+     * @var string
+     */
+    protected $county = '';
+
+    /**
+     * @var string
+     */
+    public $postcode = '';
+
+    /**
+     * @var string
+     */
+    public $country = '';
+
+    /**
+     * @var array
+     */
+    private $contacts = [];
+
+    /**
+     * @param string $houseNumber
+     * @return Address
+     */
+    public function setHouseNumber($houseNumber)
+    {
+        $this->houseNumber = $houseNumber;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHouseNumber()
+    {
+        return $this->houseNumber;
+    }
+
+    /**
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $city
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $county
+     * @return Address
+     */
+    public function setCounty($county)
+    {
+        $this->county = $county;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCounty()
+    {
+        return $this->county;
+    }
+
+    /**
+     * @param string $postcode
+     * @return Address
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * @param string $country
+     * @return Address
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param array $contacts
+     * @return Address
+     */
+    public function setContacts($contacts)
+    {
+        $this->contacts = $contacts;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContacts()
+    {
+        return $this->contacts;
+    }
 }
--- contact.php
+++ contact.php
@@ -1,8 +1,50 @@
 <?php
 
-class Contact {
-	
-	private $name = '';
-	public $email = '';
+class Contact
+{
+    /**
+     * @var string
+     */
+    private $name = '';
 
+    /**
+     * @var string
+     */
+    public $email = '';
+
+    /**
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
 }
