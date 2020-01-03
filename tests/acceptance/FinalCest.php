6fb957f65a605d93ba91ad16c530f98c7a981c24
Roberts Sukonovs <roberts.sukonovs@gmail.com>
2020-01-03 09:55:27 +0200

    Heredoc contains \n characters, but html expects <br/> tags, so it is not possible test output that way.

--- tests/acceptance/FinalCest.php
+++ tests/acceptance/FinalCest.php
@@ -4,16 +4,13 @@ class FinalCest
 {
     public function everythingWorks(AcceptanceTester $I)
     {
-        $expectedText = <<<EOT
-Address: 33 Market street, London, Greater London, EC4 MB5, GB
-Contact #1: <john@doe.com> John Doe 
-Contact #2: <anna@baker.com> Anna Baker
-Book record: #2
-Address: 22 Tower Street, SK4 1HV, GB
-Contact #1: <dane@rovens.com> Ms Dane Rovens
-EOT;
-
         $I->amOnPage('/');
-        $I->see($expectedText);
+        $I->see('Book record: #1');
+        $I->see('Address: 33 Market street, London, Greater London, EC4 MB5, GB');
+        $I->see('Contact #1: <john@doe.com> John Doe');
+        $I->see('Contact #2: <anna@baker.com> Anna Baker');
+        $I->see('Book record: #2');
+        $I->see('Address: 22 Tower Street, SK4 1HV, GB');
+        $I->see('Contact #1: <dane@rovens.com> Ms Dane Rovens');
     }
 }
